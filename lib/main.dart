87bb4c650b68e36f200b67e16a68a8f859917320
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-03-01 11:09:21 -0800

    exploring flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,22 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
-        home: MyHomePage(),
+//  ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 16, 34, 138)),
+void main() => runApp(MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Some title'),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 16, 34, 138),
       ),
-    );
-  }
-}
-
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    return Scaffold(
-      body: Column(
-        children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
-        ],
+      body: Center(
+        child: Text('data'),
       ),
-    );
-  }
-}
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text(
+          'button',
+          style: TextStyle(
+            color: Color.fromARGB(255, 93, 12, 34),
+          ),
+          ),
+          ),
+    )));
